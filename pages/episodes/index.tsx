81bea57ddcde24d0eda60901f5bc2bc17dff5e81
Andrew <dracon.24041230@mail.ru>
2025-03-29 16:24:08 +0300

    refactor

--- pages/episodes/index.tsx
+++ pages/episodes/index.tsx
@@ -1,14 +1,11 @@
-import {API} from "../../assets/api/api";
-import type {CharacterType, ResponseType} from "../../assets/api/rick-and-morty-api";
-import {PageWrapper} from "../../components/PageWrapper/PageWrapper";
-import {Card} from "../../components/Card/Card";
-import {getLayout} from "../../components/Layout/Layout";
+import {API} from "assets/api/api";
+import type {CharacterType, ResponseType} from "assets/api/rick-and-morty-api";
+import {PageWrapper} from "components/PageWrapper/PageWrapper";
+import {Card} from "components/Card/Card";
+import {getLayout} from "components/Layout/Layout";
 import type {GetStaticProps} from "next";
 
 export const getServerSideProps:GetStaticProps = async ({ }) => {
-
-
-
 
     const episodes = await API.rickAndMorty.getEpisodes();
     if(!episodes){
@@ -31,10 +28,7 @@ episodes: ResponseType<CharacterType>;
 const Episodes = ({episodes}:Props) => {
     const episodesList = episodes.results.map(character => <Card name={character.name} key={character.id} />)
 
-    return  <PageWrapper>
-
-        {episodesList && episodesList}
-    </PageWrapper>
+    return  <PageWrapper>{episodesList && episodesList}</PageWrapper>
 };
 
 Episodes.getLayout = getLayout;
